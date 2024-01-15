9d2a01468231efd0ff9dd724fe94155a8a2a04fa
Zihan-ma1610630 <zihan.efran@gmail.com>
2024-01-15 14:55:29 +0300

    .

--- payments/views.py
+++ payments/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
-    return HttpResponse ("Welcome")
+    # return HttpResponse ("Welcome")
+    return render(request, 'payments.html')
