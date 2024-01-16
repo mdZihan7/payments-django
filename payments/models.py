5975dba843ae938c03888bb61b5d190dc1c5d558
Zihan-ma1610630 <zihan.efran@gmail.com>
2024-01-16 17:12:04 +0300

    .

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Payment(models.Model):
+    pid = models.IntegerField()
+    pname = models.CharField(max_length = 20)
+    pemail = models.EmailField(max_length = 40)
